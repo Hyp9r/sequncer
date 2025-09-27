eef1496b17fc261522a381df636c580346ab2faa
Tomislav Keškić <tkeskic95@gmail.com>
2025-09-27 18:28:55 +0200

    fix: replace integer ids with uuid

--- .docker/services/postgres/init.sql
+++ .docker/services/postgres/init.sql
@@ -1,14 +1,15 @@
 CREATE TABLE sequences (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     open_tracking_enabled BOOLEAN DEFAULT FALSE,
     click_tracking_enabled BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE steps (
-    id SERIAL PRIMARY KEY,
-    sequence_id INTEGER REFERENCES sequences(id) ON DELETE CASCADE,
+CREATE TABLE sequence_steps (
+    id UUID PRIMARY KEY,
+    sequence_id UUID NOT NULL REFERENCES sequences(id) ON DELETE CASCADE,
     step_number INTEGER NOT NULL,
     content TEXT NOT NULL,
-    subject VARCHAR(255) NOT NULL
+    subject VARCHAR(255) NOT NULL,
+    CONSTRAINT unique_sequence_step_order UNIQUE(sequence_id, step_number)
 );
