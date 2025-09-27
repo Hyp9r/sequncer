b29603a0db6850ce72ef2167280f0713b18eef4e
Tomislav Keškić <tkeskic95@gmail.com>
2025-09-27 15:18:43 +0200

    chore: add database connection, logger, and .env reading, basic router

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/Netflix/go-env"
+	_ "github.com/lib/pq"
+	"github.com/rs/zerolog"
+)
+
+type AppConfig struct {
+	Port        string `env:"PORT" envDefault:"8080"`
+	SQLUsername string `env:"SQL_USERNAME"`
+	SQLPassword string `env:"SQL_PASSWORD"`
+	SQLDatabase string `env:"SQL_DATABASE"`
+	SQLHost     string `env:"SQL_HOST" envDefault:"localhost"`
+}
 
 func main() {
+	logger := zerolog.New(os.Stderr)
 	fmt.Println("Hello, Webserver!")
+
+	var Cfg AppConfig
+	_, err := env.UnmarshalFromEnviron(&Cfg)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("Failed to parse environment variables")
+	}
+
+	dbConnStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=disable", Cfg.SQLUsername, Cfg.SQLPassword, Cfg.SQLDatabase, Cfg.SQLHost)
+	db, err := sql.Open("postgres", dbConnStr)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("error connecting to postgres")
+	}
+	defer db.Close()
+
+	router := http.NewServeMux()
+
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%s", Cfg.Port),
+		Handler: router,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		logger.Fatal().Err(err).Msg("Failed to start server")
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/Hyp9r/sequncer
 
 go 1.24.0
+
+require github.com/rs/zerolog v1.34.0
+
+require (
+	github.com/Netflix/go-env v0.1.2
+	github.com/lib/pq v1.10.9
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
