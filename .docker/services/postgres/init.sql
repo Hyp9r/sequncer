842a3f1e61d7f2d0f2686402df821268e48622bd
Tomislav Keškić <tkeskic95@gmail.com>
2025-09-27 16:25:37 +0200

    feat: add step table definition

--- .docker/services/postgres/init.sql
+++ .docker/services/postgres/init.sql
@@ -4,3 +4,11 @@ CREATE TABLE sequences (
     open_tracking_enabled BOOLEAN DEFAULT FALSE,
     click_tracking_enabled BOOLEAN DEFAULT FALSE
 );
+
+CREATE TABLE steps (
+    id SERIAL PRIMARY KEY,
+    sequence_id INTEGER REFERENCES sequences(id) ON DELETE CASCADE,
+    step_number INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    subject VARCHAR(255) NOT NULL
+);
